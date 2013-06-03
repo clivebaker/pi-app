9227880f893fb4af29ea3b761ecb3eaed5fe6178
Clive Baker <clive@clivebaker.com>
2013-06-03 23:12:10 +0100

    added pi ruby

---
+++ script/pi.rb
@@ -0,0 +1,29 @@
+require 'piface'
+
+Piface.write 4, 1
+sleep 1 
+Piface.write 4, 0
+sleep 1 
+Piface.write 4, 1
+sleep 1 
+Piface.write 4, 0
+sleep 1 
+Piface.write 4, 1
+sleep 1 
+Piface.write 4, 0
+sleep 1 
+Piface.write 4, 1
+sleep 1 
+Piface.write 4, 0
+sleep 1 
+Piface.write 4, 1
+sleep 1 
+Piface.write 4, 0
+sleep 1 
+Piface.write 4, 1
+sleep 1 
+Piface.write 4, 0
+sleep 1 
+Piface.write 4, 1
+sleep 1 
+Piface.write 4, 0
