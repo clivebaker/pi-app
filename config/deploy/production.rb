94d3c326bbf65d29041efec9944ba4477e26d098
Clive Baker <clive@clivebaker.com>
2013-06-03 16:02:13 +0100

    first commit

---
+++ config/deploy/production.rb
@@ -0,0 +1,3 @@
+set :deploy_to, "/www/sites/pi-app"
+set :rails_env, "production"
+set :branch, "master"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PiApp::Application.config.secret_token = 'bc191b1c91cfb232061942ece8e4d190cb73877157ec9c981a19bb3df4c353071007626147542d7f429be42f6c1add86687f40a5481347926286b5899b2e051b'
